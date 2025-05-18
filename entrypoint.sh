a5c6d06fecbc2bafacc404eb615e09fdc06c896b
deepkothadiya13 <51197199+deepkothadiya13@users.noreply.github.com>
2025-05-18 16:39:18 +0530

    arrange proper static for css

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,5 +12,7 @@ echo "Applying migrations..."
 python manage.py makemigrations
 python manage.py migrate
 
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
 
 exec gunicorn product.wsgi:application --bind 0.0.0.0:8000 --workers 3
